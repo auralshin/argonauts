803cc43fdfeaffaebbc750f66eeb284c834bcbd7
Saksham Bhatt <sakshambhat33@mail.com>
2022-12-02 18:56:55 +0530

    added tailwind

--- exchange-ui/src/App.tsx
+++ exchange-ui/src/App.tsx
@@ -1,24 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-center">
+      <h1 className="text-3xl font-bold underline">
+        Centralized Exchange Client
+      </h1>
     </div>
   );
 }
